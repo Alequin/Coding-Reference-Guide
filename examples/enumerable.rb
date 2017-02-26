f864592548f2b54736188f8c24ff0f3c415d543d
Alequin <j-cox03@hotmail.com>
2017-02-26 19:59:59 +0000

    changed file name pdf to examples and added examples of code block use to file. Checked procs and lambda code and made minor changes to text. added link of code block examples in the code block section or ruby

---
+++ examples/enumerable.rb
@@ -0,0 +1,171 @@
+module Enumerable
+
+  def my_each(&block)
+      for val in self
+        block.call(val)
+      end
+  end
+
+  def my_each_with_index(&block)
+    index = 0
+    for val in self
+      block.call(index, val)
+      index += 1
+    end
+  end
+
+  def my_select(&block)
+
+    result = Array.new
+
+    self.my_each { |val|
+      if(block.call(val))
+        result.push(val)
+      end
+    }
+
+    return result
+
+  end
+
+  def my_all?(&block)
+
+    self.my_each { |val|
+      if(block != nil && !block.call(val))
+        return false
+      elsif(!val)
+        return false
+      end
+    }
+
+    return true
+
+  end
+
+  def my_any?(&block)
+
+    self.my_each { |val|
+      if(block != nil && block.call(val))
+        return true
+      elsif(val)
+        return true
+      end
+    }
+
+    return false
+
+  end
+
+  def my_none?(&block)
+    return !(self.my_all?(&block))
+  end
+
+  def my_count(input)
+
+    result = 0
+
+    self.my_each { |val|
+      if(val == input)
+        result += 1
+      end
+    }
+
+    puts "input"
+    return result
+
+  end
+
+  def my_count(*input)
+
+    result = 0
+
+    if(input.empty? && !block_given?)
+      if(self.class.to_s == "Range")
+        self.my_each { |a| result += 1 }
+      else
+        result = self.length
+      end
+      return result
+    end
+
+    self.my_each { |val|
+      if(block_given? && yield(val))
+        result += 1
+      else
+        if(input != nil && val == input[0])
+          result += 1
+        end
+      end
+    }
+
+    return result
+  end
+
+  def my_map(&block)
+
+    result = Array.new
+
+    self.my_each { |val|
+      result.push(block.call(val))
+    }
+
+    return result
+
+  end
+
+  def my_inject (&block)
+
+    is_hash = self.class.to_s == "Hash"
+
+    if(is_hash)
+      values = self.values
+    end
+
+    result = nil
+
+
+    self.my_each_with_index { |index, val|
+
+      input = is_hash ? values[index] : val
+
+      if(index == 0)
+        result = input
+      else
+        result = block.call(result, input)
+      end
+    }
+
+    return result
+
+  end
+
+  def Enumerable.multiply_els(array)
+    return array.my_inject { |sum, val| sum *= val }
+  end
+
+end
+
+
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+#
