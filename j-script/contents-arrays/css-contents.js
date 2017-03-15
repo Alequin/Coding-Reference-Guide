026b71081c0a5fb319fdd98bed304e8c5d5ebec7
Alequin <j-cox03@hotmail.com>
2017-03-15 18:12:14 +0000

    full j-script contents added for css

---
+++ j-script/contents-arrays/css-contents.js
@@ -0,0 +1,7 @@
+
+CONTENTS = [
+  new ContentsItem('Language in css', 'language-in-css'),
+  new ContentsItem('Z-Index', 'zindex'),
+  new ContentsItem('Vertically Align Text', 'vertically-align-text'),
+  new ContentsItem('!important priority', 'important-priority'),
+];
