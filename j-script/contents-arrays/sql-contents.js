c3d28352309a2d067e81e7daf28d04c8e098e0b1
Alequin <j-cox03@hotmail.com>
2017-03-15 17:32:54 +0000

    full j-script contents added for sql

---
+++ j-script/contents-arrays/sql-contents.js
@@ -0,0 +1,17 @@
+
+CONTENTS = [
+  new ContentsItem('Table Relationships', '#table-relationships'),
+  new ContentsItem('Foreign Keys', '#foreign-keys'),
+  new ContentsItem('LIMIT', '#limit'),
+  new ContentsItem('DISTINCT', '#distinct'),
+  new ContentsItem('Aggregate Functions', '#aggregate-functions'),
+  new ContentsItem('UPDATE with CASE', '#update-case'),
+  new ContentsItem('String Functions', '#string-functions'),
+  new ContentsItem('ALTER', '#alter'),
+  new ContentsItem('Composite Keys', '#composite-keys'),
+  new ContentsItem('Primary Keys', '#primary-keys'),
+  new ContentsItem('WHERE Clause and Conditional Operators', '#where-and-operators'),
+  new ContentsItem('NOT NULL & DEFAULT', '#not-null'),
+  new ContentsItem('Data types', '#data-types'),
+  new ContentsItem('Basic SQL and the RDBMS', '#basic-sql'),
+];
