74134c481bad9c1de41d42d3f07ead4c319a51f5
Alequin <j-cox03@hotmail.com>
2017-03-18 10:59:18 +0000

    self referencing join section added to sql

--- j-script/contents-arrays/sql-contents.js
+++ j-script/contents-arrays/sql-contents.js
@@ -1,5 +1,6 @@
 
 CONTENTS = [
+  new ContentsItem('Self Referencing Join', 'self-join'),
   new ContentsItem('Subquery', 'subquery'),
   new ContentsItem('Outer Join', 'outer-join'),
   new ContentsItem('Inner Join', 'inner-join'),
