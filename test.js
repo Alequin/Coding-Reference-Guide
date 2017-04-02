bae217d5ed9143e8dfa00d169cdb8f7852ccf3a1
J Cox <J Cox>
2017-04-02 10:27:22 +0100

    added DOM section to javascript

--- test.js
+++ test.js
@@ -1,8 +1,5 @@
 
 //var newWindow = open("https://www.w3schools.com/js/js_window.asp");
-alert("hello");
-y = confirm("hello?");
-x = prompt("hello!");
 
-alert(y);
-alert(x);
+alert(outerHeight);
+alert(innerHeight);
