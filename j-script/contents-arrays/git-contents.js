f45f358432336efa0b064dd13aa5891ac2d55492
Alequin <j-cox03@hotmail.com>
2017-04-17 16:58:04 +0100

    Added sections to git. What is git and Common Commands: add

--- j-script/contents-arrays/git-contents.js
+++ j-script/contents-arrays/git-contents.js
@@ -2,4 +2,6 @@
 CONTENTS = [
   new ContentsItem('Removing Commits', 'remove-commits'),
   new ContentsItem('Rename Branch', 'rename-branch'),
+  new ContentsItem('Common Commands: add', 'add'),
+  new ContentsItem('What Is Git', 'git-summary'),
 ];
