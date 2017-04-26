51f64c5719014ab64fc0e9e98c84a76986d84644
Alequin <j-cox03@hotmail.com>
2017-04-26 17:30:44 +0100

    Added description of html section to html

--- j-script/contents-arrays/html-contents.js
+++ j-script/contents-arrays/html-contents.js
@@ -3,4 +3,5 @@ CONTENTS = [
   new ContentsItem('Tags to Remember', 'tags-to-remember'),
   new ContentsItem('Make Anchor Tags open a New Tab', 'anchor-open-new-tab'),
   new ContentsItem('HTML Entities', 'html-entities'),
+  new ContentsItem('What is HTML', 'html'),
 ];
