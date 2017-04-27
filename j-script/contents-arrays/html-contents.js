002ac1e0f8f25344984cda7e8525dcb542dad05d
Alequin <j-cox03@hotmail.com>
2017-04-27 11:21:26 +0100

    Added to basic html layout section in html. Added conents item for Declaring Language section in html.

--- j-script/contents-arrays/html-contents.js
+++ j-script/contents-arrays/html-contents.js
@@ -1,6 +1,7 @@
 
 CONTENTS = [
   new ContentsItem('Tags to Remember', 'tags-to-remember'),
+  new ContentsItem('Declaring Language', 'language'),
   new ContentsItem('Make Anchor Tags open a New Tab', 'anchor-open-new-tab'),
   new ContentsItem('HTML Entities', 'html-entities'),
   new ContentsItem('Basic HTML Layout', 'basic-layout'),
