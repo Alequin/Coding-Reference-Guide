3d65e82b4a0adbb493daa79aaee865cb53c57ffa
James Cox <j-cox03@hotmail.com>
2017-05-03 15:21:37 +0100

    Added URI, URL & URN section to workings of the internet.

--- j-script/contents-arrays/internet-workings-contents.js
+++ j-script/contents-arrays/internet-workings-contents.js
@@ -1,5 +1,6 @@
 
 CONTENTS = [
+  new ContentsItem('URI, URL & URN', 'uri-url-urn'),
   new ContentsItem('IP address', 'ip-address'),
   new ContentsItem('Basics of The Internet', 'internet-basics'),
 ];
