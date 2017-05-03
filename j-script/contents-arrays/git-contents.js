8681284a9a8f6238fec77ebabb47d2a8ea8c4d07
James Cox <j-cox03@hotmail.com>
2017-05-03 20:36:22 +0100

    Added init and status sections to git.

--- j-script/contents-arrays/git-contents.js
+++ j-script/contents-arrays/git-contents.js
@@ -3,5 +3,7 @@ CONTENTS = [
   new ContentsItem('Removing Commits', 'remove-commits'),
   new ContentsItem('Rename Branch', 'rename-branch'),
   new ContentsItem('Common Commands: add', 'add'),
+  new ContentsItem('Common Commands: status', 'status'),
+  new ContentsItem('Common Commands: init', 'init'),
   new ContentsItem('What Is Git', 'git-summary'),
 ];
