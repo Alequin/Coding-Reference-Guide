b32d0b2547f3ef8f52375bb545f0aca9e9dec97f
James Cox <j-cox03@hotmail.com>
2017-05-04 11:04:28 +0100

    Added multiple sections to git based around common commands. Added to git-add section in git

--- j-script/contents-arrays/git-contents.js
+++ j-script/contents-arrays/git-contents.js
@@ -2,7 +2,10 @@
 CONTENTS = [
   new ContentsItem('Removing Commits', 'remove-commits'),
   new ContentsItem('Rename Branch', 'rename-branch'),
-  new ContentsItem('Common Commands: add', 'add'),
+  new ContentsItem('Common Commands: checkout', 'checkout'),
+  new ContentsItem('Common Commands: show', 'show'),
+  new ContentsItem('Common Commands: diff', 'diff'),
+  new ContentsItem('Common Commands: commit', 'commit'),
   new ContentsItem('Common Commands: status', 'status'),
   new ContentsItem('Common Commands: init', 'init'),
   new ContentsItem('What Is Git', 'git-summary'),
