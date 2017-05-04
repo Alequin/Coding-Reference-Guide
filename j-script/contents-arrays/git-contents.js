df496aeef597e1003aa1cbf628b274353d893cf2
James Cox <j-cox03@hotmail.com>
2017-05-04 22:36:54 +0100

    Added config section to git.

--- j-script/contents-arrays/git-contents.js
+++ j-script/contents-arrays/git-contents.js
@@ -3,6 +3,7 @@ CONTENTS = [
   new ContentsItem('Removing Commits', 'remove-commits'),
   new ContentsItem('Rename Branch', 'rename-branch'),
   new ContentsItem('Branches', 'branches'),
+  new ContentsItem('Config', 'config'),
   new ContentsItem('Common Commands: checkout', 'checkout'),
   new ContentsItem('Common Commands: show', 'show'),
   new ContentsItem('Common Commands: diff', 'diff'),
