59113fbdb96500214d79244bbb940e504802974d
James Cox <j-cox03@hotmail.com>
2017-05-04 11:07:29 +0100

    Added section git-reset to git

--- j-script/contents-arrays/git-contents.js
+++ j-script/contents-arrays/git-contents.js
@@ -6,6 +6,8 @@ CONTENTS = [
   new ContentsItem('Common Commands: show', 'show'),
   new ContentsItem('Common Commands: diff', 'diff'),
   new ContentsItem('Common Commands: commit', 'commit'),
+  new ContentsItem('Common Commands: reset', 'reset'),
+  new ContentsItem('Common Commands: add', 'add'),
   new ContentsItem('Common Commands: status', 'status'),
   new ContentsItem('Common Commands: init', 'init'),
   new ContentsItem('What Is Git', 'git-summary'),
