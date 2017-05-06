053fa13d89f4393db6e355abfc1cdc0aaa37bf2e
James Cox <j-cox03@hotmail.com>
2017-05-06 12:53:56 +0100

    Page Edited: Unix and shell commands Section Added: Redirection   Edit Made:     - Contents item made for new section     - Source for info that will be added input.     - update date added

--- j-script/contents-arrays/console-contents.js
+++ j-script/contents-arrays/console-contents.js
@@ -1,5 +1,6 @@
 
 CONTENTS = [
+  new ContentsItem('Redirection', 'redirect'),
   new ContentsItem('Print and Search File Contents', 'file-contents'),
   new ContentsItem('Basic Commands', 'basic-commands'),
   new ContentsItem('What is UNIX', 'unix'),
