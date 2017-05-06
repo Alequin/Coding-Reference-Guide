659b833a9e61e3ae602351df0bbee8a7f7c1c181
James Cox <j-cox03@hotmail.com>
2017-05-06 16:06:32 +0100

    Page Edited: Unix and shell commands Section Added:     - Nano     - Alias     - Environment Variables

--- j-script/contents-arrays/console-contents.js
+++ j-script/contents-arrays/console-contents.js
@@ -1,5 +1,8 @@
 
 CONTENTS = [
+  new ContentsItem('Environment Variables', 'env-vars'),
+  new ContentsItem('Alias', 'alias'),
+  new ContentsItem('Nano', 'nano'),
   new ContentsItem('Redirection', 'redirect'),
   new ContentsItem('Print and Search File Contents', 'file-contents'),
   new ContentsItem('Basic Commands', 'basic-commands'),
