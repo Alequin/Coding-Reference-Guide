229da5251edaa449fc24cfb2bcbb0535887d5e99
James Cox <j-cox03@hotmail.com>
2017-05-07 13:08:18 +0100

    Page Edited: Ruby Section Edited: Redirection & Pipe Characters     Edit Made:       - Added example of chained redirection in terminal
    Other: Altered examples for ruby redirection

--- examples/ruby/ruby-input.rb
+++ examples/ruby/ruby-input.rb
@@ -2,7 +2,7 @@ def print_input(input)
   puts input
 end
 
-print_input(gets.chomp)
-print_input(gets.chomp)
-print_input(gets.chomp)
-print_input(gets.chomp)
+print_input(gets)
+print_input(gets)
+print_input(gets)
+print_input(gets)
--- examples/ruby/ruby-output.rb
+++ examples/ruby/ruby-output.rb
@@ -1,1 +1,4 @@
 puts "hello"
+puts "how"
+puts "are"
+puts "you"
