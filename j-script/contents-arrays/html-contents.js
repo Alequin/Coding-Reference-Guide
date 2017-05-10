6981572aef91f48c4d26dbf6d2adcb2d548463e0
James Cox <j-cox03@hotmail.com>
2017-05-10 18:17:07 +0100

    Edits: HTML
    Page Edited: HTML
    Section: Link Tag (CSS)
      Edited:
        - Added Section to html
    
    Other: Added to info-to-add

--- j-script/contents-arrays/html-contents.js
+++ j-script/contents-arrays/html-contents.js
@@ -4,6 +4,7 @@ CONTENTS = [
   new ContentsItem('Declaring Language', 'language'),
   new ContentsItem('Make Anchor Tags open a New Tab', 'anchor-open-new-tab'),
   new ContentsItem('HTML Entities', 'html-entities'),
+  new ContentsItem('Link Tag (CSS)', 'link-tag'),
   new ContentsItem('Basic HTML Layout', 'basic-layout'),
   new ContentsItem('What is HTML', 'html'),
 ];
