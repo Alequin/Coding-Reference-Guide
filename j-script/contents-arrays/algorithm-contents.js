79536a5696ad90d3022df08e26f67c23be23d607
James Cox <j-cox03@hotmail.com>
2017-05-21 20:28:11 +0100

    Edits: Useful Algorithms
    Page Edited: Useful Algorithms
    Section: Convert a Decimal Number To Hexadecimal
      Edited:
        - Section added
        - Checked grammar

--- j-script/contents-arrays/algorithm-contents.js
+++ j-script/contents-arrays/algorithm-contents.js
@@ -1,5 +1,6 @@
 
 CONTENTS = [
+  new ContentsItem('Convert a Decimal Number To Hexadecimal', 'decimal-to-hex'),
   new ContentsItem('Binary Search', 'binary-search'),
   new ContentsItem('Quick Sort', 'quick-sort'),
   new ContentsItem('Merge Sort', 'merge-sort'),
