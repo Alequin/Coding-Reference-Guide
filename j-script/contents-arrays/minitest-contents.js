488f9ba3bba62271c36d44926f8a331a7e53c35b
James Cox <j-cox03@hotmail.com>
2017-05-27 10:36:25 +0100

    Added to title on minitest page (Basic Layout and Use (Assertions)) to improve section description

--- j-script/contents-arrays/minitest-contents.js
+++ j-script/contents-arrays/minitest-contents.js
@@ -1,6 +1,6 @@
 
 CONTENTS = [
   new ContentsItem('Assertion Methods', 'assertions'),
-  new ContentsItem('Basic Layout and Use', 'basic-layout-and-use'),
+  new ContentsItem('Basic Layout and Use (Assertions)', 'basic-layout-and-use'),
   new ContentsItem('What is Minitest and Testing', 'what-is-minitest'),
 ];
