35447989215052b209cac2dedb437025aec2197d
James Cox <j-cox03@hotmail.com>
2017-06-14 21:19:58 +0100

    Started new section "Query Results" on pg gem template

--- j-script/contents-arrays/ruby_gem_pg_contents.js
+++ j-script/contents-arrays/ruby_gem_pg_contents.js
@@ -1,5 +1,6 @@
 
 CONTENTS = [
+  new ContentsItem('Query Results', 'query_results'),
   new ContentsItem('Running SQL Commands', 'run_commands'),
   new ContentsItem('Connect To The Database', 'connecting'),
   new ContentsItem('What is PG?', 'what_is_pg'),
