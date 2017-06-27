5c55a2cec2c665209535d11cf48f4a72131d2190
James Cox <j-cox03@hotmail.com>
2017-06-27 17:43:35 +0100

    Added new blank section to uml template on uml notation. Made contents item for this section

--- j-script/contents-arrays/uml-contents.js
+++ j-script/contents-arrays/uml-contents.js
@@ -1,6 +1,7 @@
 
 CONTENTS = [
   new ContentsItem('Class Diagrams', 'class-diagrams'),
+  new ContentsItem('UML Notation', 'uml-notation'),
   new ContentsItem('Diagrams', 'diagrams'),
   new ContentsItem('What is UML', 'what_is_uml'),
 ];
