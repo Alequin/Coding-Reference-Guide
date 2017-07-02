ef71abdb181f09241e6672b09e167b4e3c68e484
James Cox <j-cox03@hotmail.com>
2017-07-02 14:11:19 +0100

    Java: moved template section on annotations into java

--- j-script/contents-arrays/java-contents.js
+++ j-script/contents-arrays/java-contents.js
@@ -1,5 +1,6 @@
 
 CONTENTS = [
+  new ContentsItem('Annotations', 'annotations'),
   new ContentsItem('Exceptions and Error Handling', 'exceptions'),
   new ContentsItem('Interfaces', 'interfaces'),
   new ContentsItem('Abstract Classes', 'abstract-class'),
