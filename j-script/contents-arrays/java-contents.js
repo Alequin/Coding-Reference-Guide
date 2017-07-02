94a4803b6471a0927b4a0545d722cf1c8136d3f4
James Cox <j-cox03@hotmail.com>
2017-07-02 10:33:32 +0100

    java: moved exceptions section into java and made contents items.

--- j-script/contents-arrays/java-contents.js
+++ j-script/contents-arrays/java-contents.js
@@ -1,5 +1,6 @@
 
 CONTENTS = [
+  new ContentsItem('Exceptions and Error Handling', 'exceptions'),
   new ContentsItem('Interfaces', 'interfaces'),
   new ContentsItem('Abstract Classes', 'abstract-class'),
   new ContentsItem('What is Java', 'what-is-java'),
