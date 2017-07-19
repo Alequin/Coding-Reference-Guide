b0ddb3a679359bcbd0690a2f71244457ff8212f9
James Cox <j-cox03@hotmail.com>
2017-07-19 16:39:33 +0100

    Added to testing section

--- j-script/contents-arrays/testing-contents.js
+++ j-script/contents-arrays/testing-contents.js
@@ -1,5 +1,7 @@
 
 CONTENTS = [
+  new ContentsItem('White Box Testing', 'white-box-testing'),
+  new ContentsItem('Black Box Testing', 'black-box-testing'),
   new ContentsItem('Dynamic testing', 'dynamic-testing'),
   new ContentsItem('Static Testing', 'static-testing'),
 ];
