c2e46ec0e534f9f96a35e56de9bf6471ad688e61
James Cox <j-cox03@hotmail.com>
2017-07-19 14:49:31 +0100

    Made multiple section in working template

---
+++ j-script/contents-arrays/key-terms-contents.js
@@ -0,0 +1,16 @@
+
+CONTENTS = [
+  new ContentsItem('Hash', 'hash'),
+  new ContentsItem('Array', 'array'),
+  new ContentsItem('Boolean', 'boolean'),
+  new ContentsItem('Floating point', 'floating-point'),
+  new ContentsItem('Integer', 'integer'),
+  new ContentsItem('String', 'string'),
+  new ContentsItem('File Handling', 'file-handling'),
+  new ContentsItem('Predefined Functions', 'predefined-functions'),
+  new ContentsItem('Operator', 'operator'),
+  new ContentsItem('Operand', 'operand'),
+  new ContentsItem('Expression', 'expression'),
+  new ContentsItem('Selection', 'selection'),
+  new ContentsItem('Sequence', 'sequence'),
+];
