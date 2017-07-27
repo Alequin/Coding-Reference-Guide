256a5d0e89f068f80aca349bafa2d46a2d1bdfcb
James Cox <j-cox03@hotmail.com>
2017-07-28 00:33:21 +0100

    extended make_working_template.rb helper

--- helpers/make_working_template.rb
+++ helpers/make_working_template.rb
@@ -1,3 +1,5 @@
 currentFile = File.expand_path(File.dirname(__FILE__))
-targetFile = currentFile + "/../html/"
-system("cp #{currentFile}/template.html #{targetFile}/working_template.html")
+targetFile = currentFile + "/../html//working_template.html"
+system("cp #{currentFile}/template.html #{targetFile}")
+system("git add #{targetFile}")
+system("git commit -m 'reset working template'")
